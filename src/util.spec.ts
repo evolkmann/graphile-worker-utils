9802bebca26f38af4e37e18d5e101dc7904f786f
Enzo Volkmann <enzo@exportarts.io>
2022-07-09 15:44:15 +0200

    chore: add tests

---
+++ src/util.spec.ts
@@ -0,0 +1,26 @@
+import { numberOrDefault, stringOrNull } from './util';
+
+describe('util', () => {
+    describe('stringOrNull', () => {
+        it('should handle valid strings', () => {
+            expect(stringOrNull('str')).toEqual(`'str'`);
+        });
+        it('should handle nullish values', () => {
+            expect(stringOrNull(null)).toEqual(`null`);
+            expect(stringOrNull(undefined)).toEqual(`null`);
+        });
+    });
+
+    describe('numberOrDefault', () => {
+        it('should handle valid numbers', () => {
+            expect(numberOrDefault(-1)).toEqual(`-1`);
+            expect(numberOrDefault(0)).toEqual(`0`);
+            expect(numberOrDefault(1)).toEqual(`1`);
+            expect(numberOrDefault(0.5)).toEqual(`0.5`);
+        });
+        it('should handle nullish values', () => {
+            expect(numberOrDefault(null)).toEqual(`default`);
+            expect(numberOrDefault(undefined)).toEqual(`default`);
+        });
+    });
+});
