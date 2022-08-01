783b3fbef2f209e5f55bffea91e5cb2e59381048
Enzo Volkmann <enzo@exportarts.io>
2022-08-01 13:53:39 +0200

    fix(manager): make queue optional

--- src/manager.ts
+++ src/manager.ts
@@ -6,7 +6,7 @@ export abstract class GraphileQueueManager {
 
     protected constructor(
         private readonly workerUtils: WorkerUtils,
-        private readonly queueName: string
+        private readonly queueName?: string
     ) {}
 
     protected async enqueue<T = unknown>(jobType: string, payload?: T, options: TaskOptions = {}): Promise<Job> {
